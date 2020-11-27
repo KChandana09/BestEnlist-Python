d3b0c43212d63d6100ec590c0d910ba30fd894b7
KChandana09 <chandanakattamanchi2017@gmail.com>
2020-11-27 21:09:38 +0530

    Task with solution

---
+++ Day5/Task1.py
@@ -0,0 +1,10 @@
+x = int(input("Enter first integer:"))
+y = int(input("Enter second integer:"))
+
+def func(a,b):
+    print("Addition of two numbers is ",a+b)
+    print("Subtraction of two numbers is ",a-b)
+    print("Division of two numbers is ",a/b)
+    print("Multiplication of two numbers is ",a*b)
+
+func(x,y)
---
+++ Day5/Task2.py
@@ -0,0 +1,9 @@
+def covid(name, temp=98):
+    print("Patient name is",name, "and his body temperature is",temp,"degree")
+
+covid("Ram")
+
+
+
+
+
